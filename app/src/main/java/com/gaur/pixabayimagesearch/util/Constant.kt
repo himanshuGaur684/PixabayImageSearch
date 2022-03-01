a728b6ed8f082c16850749b6d8a4cc50d2a11de2
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2022-03-01 12:36:55 +0530

    Initial Commit

--- app/src/main/java/com/gaur/pixabayimagesearch/util/Constant.kt
+++ app/src/main/java/com/gaur/pixabayimagesearch/util/Constant.kt
@@ -3,6 +3,6 @@ package com.gaur.pixabayimagesearch.util
 object Constant {
 
     const val BASE_URL ="https://pixabay.com/"
-    const val KEY="22524948-6c7bec03f58886bc4641a5cf9"
+    const val KEY=""
 
 }
